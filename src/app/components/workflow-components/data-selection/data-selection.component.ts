eed067f395a9f37f31a91ec6fc2ea2647b808029
rajiniw95 <rajiniw95@gmail.com>
2019-09-09 11:15:44 +0530

    modify drop rows and drop columns

--- src/app/components/workflow-components/data-selection/data-selection.component.ts
+++ src/app/components/workflow-components/data-selection/data-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-data-selection',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataSelectionComponent implements OnInit {
 
-  constructor() { }
+  public modalRef: BsModalRef; // {1}
+  constructor(private modalService: BsModalService) { }
   public isCollapsed1 = true;
   public isCollapsed = true;
+
+  public openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template); // {3}
+  }
+
+  public closeModal() {
+    this.modalRef.hide(); // {3}
+
+  }
+
+  public save() {
+    this.modalRef.hide(); // {3}
+    // this.passEntry.emit(this.name);
+    // this.router.navigate([`webApp/workflow`]);
+  }
 
   ngOnInit() {
   }
